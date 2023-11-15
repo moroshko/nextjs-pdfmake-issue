9060765d249a400ad9b1acd65a21f3542757c559
Misha Moroshko <michael.moroshko@gmail.com>
2023-11-15 11:19:32 +1100

    Add pdfmake to route handler

---
+++ app/api/hello/route.ts
@@ -0,0 +1,11 @@
+import PdfPrinter from "pdfmake";
+
+export async function GET() {
+  try {
+    const printer = new PdfPrinter({});
+
+    return Response.json({ message: "ok" });
+  } catch (error) {
+    return Response.json({ error: "something went" });
+  }
+}
